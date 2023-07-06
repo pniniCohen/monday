af1637f1798aa36361d35a422e240702b4535fa3
Ye-Bo <yehudit.bodner@i-rox.net>
2023-07-06 16:56:26 +0300

    handle react warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-//import { Provider } from 'react-redux';
-//import { store } from './app/store/store';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  // <React.StrictMode> 
-  //<Provider store={store}>
-  //<ScrollToTop>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
-  // </ScrollToTop>
-  // </Provider>
-  // </React.StrictMode>
-  document.getElementById('root')
-);
-
+window.addEventListener("DOMContentLoaded", function (e) {
+  const container = document.getElementById('root');
+  const root = createRoot(container!);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>);
+});
 reportWebVitals();
-
-
-
-
-// // index.js
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
